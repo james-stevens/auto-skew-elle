fe7067a17616be8c09b8318f835e6c99deb2321d
James Stevens <james@jrcs.net>
2021-02-24 18:21:53 +0000

    automatically join tables for where clauses

--- auto_sql.py
+++ auto_sql.py
@@ -72,10 +72,12 @@ def find_best_index(idxes):
     return idx
 
 
-def add_data(data, is_int):
+def add_data(data, this_col):
     """ convert {data} to SQL string """
-    if is_int:
+    if this_col["is_plain_int"]:
         return str(int(data))
+    if this_col["type"] == "boolean":
+        return "1" if data else "0"
     if not isinstance(data, str):
         data = str(data)
     return "unhex('" + "".join([hex(ord(a))[2:] for a in data]) + "')"
@@ -128,18 +130,70 @@ def clean_col_data(data, table, column):
     return data
 
 
+def find_join_column(src_table,dst_table):
+    for col in schema[src_table]["columns"]:
+        this_col = schema[src_table]["columns"][col]
+        if "join" in this_col and this_col["join"]["table"] == dst_table:
+            return col
+    return None
+
+
+def find_foreign_column(sql_joins, src_table, col):
+    dst = col.split(".")
+
+    if len(dst) != 2:
+        flask.abort(400,{"error":"Invalid column name `{col}`".format(col=col)})
+
+    if dst[0] in schema[src_table]["columns"] and "join" in schema[src_table]["columns"][dst[0]]:
+        src_col = schema[src_table]["columns"][dst[0]]
+        alias = "__zz__" + dst[0]
+        col = alias + "." + dst[1]
+        if alias in sql_joins:
+            return col, src_col["join"]["table"]
+
+        sql_joins[alias] = "join {dsttbl} {alias} on({srctbl}.{srccol}={alias}.{dstcol})".format(
+            dsttbl = src_col["join"]["table"],
+            dstcol = src_col["join"]["column"],
+            alias=alias, srctbl = src_table, srccol = dst[0])
+        return col, src_col["join"]["table"]
+
+    col_name = find_join_column(src_table,dst[0])
+    if col_name is None:
+        flask.abort(400,{"error":"Could not find a join for `{col}` to `{tbl}`".format(col=col,tbl=src_table)})
+
+    alias = "__zz__" + col_name
+    col = alias + "." + dst[1]
+
+    if alias in sql_joins:
+        return col, dst[0]
+
+    this_col = schema[src_table]["columns"][col_name]
+    sql_joins[alias] = "join {dsttbl} {alias} on({srctbl}.{srccol}={alias}.{dstcol})".format(
+        alias=alias,srctbl=src_table,srccol=col_name,dsttbl=dst[0],dstcol=this_col["join"]["column"])
+
+    return col, dst[0]
+
+
+
 def where_clause(table, where_data):
     """ convert the {where_data} JSON into SQL """
     if where_data is None:
         return ""
 
     this_cols = schema[table]["columns"]
+    sql_joins = {}
     where = []
     for ask_item in ASKS:
         if ask_item not in where_data:
             continue
-        for col in where_data[ask_item]:
-            if col not in this_cols:
+
+        for where_itm in where_data[ask_item]:
+            alias = table
+            tbl = table
+            col = where_itm
+            if where_itm.find(".") >= 0:
+                col, tbl = find_foreign_column(sql_joins, table, col)
+            elif col not in this_cols:
                 flask.abort(
                     400, {
                         "error":
@@ -147,17 +201,13 @@ def where_clause(table, where_data):
                             col=col, tbl=table)
                     })
 
-        for col in this_cols:
-            if col in where_data[ask_item]:
-                clause = []
-                for itm in clean_list_string(where_data[ask_item][col]):
-                    clause.append(
-                        col + ASKS[ask_item] +
-                        add_data(itm, this_cols[col]["is_plain_int"]))
+            clause = []
+            for itm in clean_list_string(where_data[ask_item][where_itm]):
+                clause.append(col + ASKS[ask_item] + add_data(itm, schema[tbl]["columns"][col.split(".")[1]]))
 
-                where.append("(" + " or ".join(clause) + ")")
+            where.append("(" + " or ".join(clause) + ")")
 
-    return " where " + " and ".join(where) if len(where) > 0 else ""
+    return " ".join([ sql_joins[x] for x in sql_joins]) + (" where " + " and ".join(where)) if len(where) > 0 else ""
 
 
 def plain_value(data):
@@ -169,11 +219,6 @@ def plain_value(data):
     if ":join:" in data:
         return data[data[":join:"].split(".")[1]]
     return str(data)
-
-
-def unique_id(best_idx, row):
-    """ format the index item for {row} """
-    return ".".join([plain_value(row[idx]) for idx in best_idx])
 
 
 def include_for_join(data):
@@ -192,9 +237,9 @@ def load_all_joins(need):
         src = item.split(".")
         sql = "select * from " + src[0] + " where " + item + " in ("
 
-        is_int = schema[src[0]]["columns"][src[1]]["is_plain_int"]
+        this_col = schema[src[0]]["columns"][src[1]]
 
-        clauses = [add_data(d, is_int) for d in need[item]]
+        clauses = [add_data(d, this_col) for d in need[item]]
 
         if len(clauses) <= 0:
             continue
@@ -274,6 +319,11 @@ def add_join_data(data, join_data, which):
                         data[table][row][col][":join:"] = target
 
 
+def unique_id(best_idx, row):
+    """ format the index item for {row} """
+    return "|".join([plain_value(row[idx]) for idx in best_idx])
+
+
 cnx = mysql_connect()
 schema = mysql_schema.load_db_schema(cnx)
 application = flask.Flask("MySQL-Rest/API")
@@ -314,8 +364,9 @@ def get_table_row(table_name):
         flask.abort(404, {"error": "Table not found"})
 
     sent = flask.request.json if flask.request.json is not None else {}
-    sql = "select * from " + table_name
+    sql = "select {tbl}.* from {tbl} ".format(tbl=table_name)
     sql = sql + where_clause(table_name, sent)
+    print(">>>>",sql)
 
     cnx.query(sql)
     res = cnx.store_result()
@@ -345,8 +396,12 @@ def get_table_row(table_name):
     ret = {table_name: {unique_id(idx_cols, tmp): tmp for tmp in ret}}
 
     if "join" in sent:
-        handle_joins(ret, clean_list_string(sent["join"]),
-                     ("join-basic" in sent and sent["join-basic"]))
+        join = sent["join"]
+        if isinstance(join,bool):
+            join = [":all:"] if join else None
+        if join is not None:
+            handle_joins(ret, clean_list_string(join),
+                         ("join-basic" in sent and sent["join-basic"]))
 
     return json.dumps(ret), 200
 
